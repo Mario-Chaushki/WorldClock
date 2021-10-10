60be25893b806f5697b306d2a1533b17b0056cf5
Ivan.pamukliyski@students.softuni.bg <Ivan.pamukliyski@students.softuni.bg>
2021-10-10 21:31:46 +0300

    Timer changes

--- WorldClock/Forms/TimerForm.cs
+++ WorldClock/Forms/TimerForm.cs
@@ -12,9 +12,50 @@
 
     public partial class TimerForm : Form
     {
+        int timerCount = 0;
+        Timer timer = new Timer();
+        string endTime = string.Empty;
         public TimerForm()
         {
             InitializeComponent();
+            this.startButton.Click += StartTimerOnClick;
+            this.pauseButton.Click += PauseButton;
         }
+
+        private void PauseButton(object sender, EventArgs e)
+        {
+            timer.Stop();
+            this.startButton.Enabled = true;
+        }
+
+        private void StartTimerOnClick(object sender, EventArgs e)
+        {
+            this.timerTextBox.Enabled = false;
+            this.startButton.Enabled = false;
+            endTime = this.timerTextBox.Text;
+            timer.Interval = 1000; // 1 sec
+            if (timerCount == 0)
+            {
+                timer.Tick += new EventHandler(OnTick);
+            }
+            timerCount = int.Parse(this.timerTextBox.Text);
+            timer.Start();
+            Console.WriteLine("started timer");
+
+        }
+        public void OnTick(object sender, EventArgs e)
+        {
+
+            timerCount--;
+            if (timerCount == 0)
+            {
+                timer.Stop();
+                this.timerTextBox.Text = "Done";
+                return;
+            }
+
+            this.timerTextBox.Text = timerCount.ToString();
+        }
+
     }
 }
